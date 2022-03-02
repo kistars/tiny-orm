a64b8ef9b2fde1d8baa25901c39e3cb396afdf40
孙帅 <sunshuai@rockontrol.com>
2022-03-02 10:48:27 +0800

    write test method

--- orm_test.go
+++ orm_test.go
@@ -7,17 +7,55 @@ import (
 )
 
 func TestOrmEngine_Insert(t *testing.T) {
-	_, _ = Engine.Table("user").Insert(User{
+	n, err := Engine.Table("user").Insert(User{
 		Name: "Tom",
 		Age:  12,
 	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(n)
 }
 
 func TestOrmEngine_Where(t *testing.T) {
-	Engine.Table("userinfo").Where("username", "EE").OrWhere("departname", "22").OrWhere("status", 1)
+	Engine.Table("user").Where("username", "EE").OrWhere("departname", "22").OrWhere("status", 1)
 	fmt.Println(Engine.WhereParam + Engine.OrWhereParam)
 }
 
 func TestOrmEngine_Select(t *testing.T) {
+	i, err := Engine.Table("user").Select()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(i)
+}
 
+func TestOrmEngine_Update(t *testing.T) {
+	i, err := Engine.Table("user").Where("name", "Tom").Update("name", "Jack")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(i)
+}
+
+func TestOrmEngine_Delete(t *testing.T) {
+	i, err := Engine.Table("user").Where("name", "Tom").Delete()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(i)
+}
+
+func TestOrmEngine_Find(t *testing.T) {
+	u := []User{}
+	err := Engine.Table("user").Where("name", "Jack").Find(&u)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(u)
 }
